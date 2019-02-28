61969235d0b05c7f31ccaacea1905ddebab14275
github username <github email address>
2019-02-28 16:06:53 -0600

    Done

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,76 @@
-class Startup
+class Startup 
+    attr_accessor :name
+    attr_reader :founder, :domain
+
+    @@all = []
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+  def pivot(name, domain)
+    @name = name
+    @domain = domain
+    self
+  end
+
+
+  def find_by_founder (name)
+    @@all.find do |startup|
+        startup.founder == name # the founder has to be an instance
+    end
+  end
+
+  def self.domains
+    results = []
+    @@all.map do |startup|
+        results << startup.domain
+    end
+    results.uniq   # no duplicate
+  end
+
+  def sign_contract(venture_capitalist, type, amount)
+    FundingRound.new(amount, venture_capitalist, self, type)
+  end
+   
+  def this_startup_round
+    FundingRound.all.select {|round| round.startup == self}
+  end
+
+  def num_funding_rounds
+    self.this_startup_round.length
+  end
+
+  def total_funds
+    total = 0
+    self.this_startup_round.map do |round|
+        total += round.amount
+    end
+    total
+  end
+
+  def investors
+      caplitalists = []
+    self.this_startup_round.map do |round|
+      caplitalists << round.venture_capitalist.name
+    end
+      caplitalists.uniq
+  end
+
+  def big_investors
+    self.investors.select do|investor| 
+      VentureCapitalist.tres_commas_club.map do |caplitalist|
+        investor == caplitalist.name
+      end
+    end
+  end
 
 end
+
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,64 @@
-class VentureCapitalist
+class VentureCapitalist 
+    attr_accessor :name, :total_worth 
 
+    @@all = []
+  def initialize(name, total_worth)
+    @name = name
+    @total_worth = total_worth
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.tres_commas_club #they have to be in this club first
+    @@all.select do |capitalist|
+        capitalist.total_worth > 1000000000 
+    end
+  end
+
+  def offer_contract(startup, type, amount)
+    FundingRound.new(amount, self, startup, type)
+  end
+
+  def funding_rounds
+    FundingRound.all.select do |round|
+        round.venture_capitalist == self
+    end
+  end
+
+  def portfolio
+    startup = nil
+    self.funding_rounds.select do |current_startup|
+        if current_startup.startup != startup
+            startup = current_startup.startup
+        end
+    end
+    startup
+  end
+
+  def biggest_investment
+    this_amount = 0
+    largest_round = nil
+    self.funding_rounds.map do |round|
+     if round.amount > this_amount
+        this_amount = round.amount
+        largest_round = round
+     end
+    end
+    largest_round
+  end
+
+  def invested(domain)
+    results = self.funding_rounds.select do |round|
+       round.startup.domain == domain
+    end
+    total = 0
+    results.map do |round|
+    total += round.amount
+    end
+    total
+  end
 end
+
