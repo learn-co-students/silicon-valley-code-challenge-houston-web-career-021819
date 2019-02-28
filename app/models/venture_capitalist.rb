8976aa935c380467d6f8f10df9c7d8e468c2cf29
moranja <jeffadammoran@gmail.com>
2019-02-28 14:05:14 -0600

    done

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,43 @@
 class VentureCapitalist
+  @@all = []
+  attr_accessor :name, :total_worth
 
+  def initialize (name:, total_worth:)
+    self.name = name
+    self.total_worth = total_worth
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.tres_commas_club
+    self.all.select {|vcs| vcs.total_worth > 999999999}
+  end
+
+  def offer_contract (startup, type, investment)
+    FundingRound.new(startup: startup, v_c: self, type: type, investment: investment)
+  end
+
+  def funding_rounds
+    FundingRound.all.select {|fr| fr.v_c == self}
+  end
+
+  def portfolio
+    (self.funding_rounds.map {|fr| fr.startup}).uniq
+  end
+
+  def biggest_investment
+    self.funding_rounds.max_by {|fr| fr.investment}
+  end
+
+  def invested (domain)
+    amount_invested = ((self.funding_rounds.select {|fr| fr.domain == domain}).map {|fr| fr.investment}).inject(:+)
+    if amount_invested
+      amount_invested
+    else
+      0
+    end
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,30 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+facebook = Startup.new(name: 'Facebook', founder: 'Mark Zuckerburg', domain: 'tech')
+spotify = Startup.new(name: 'Spotify', founder: 'Mr. Spotify', domain: 'tech')
+twitter = Startup.new(name: 'Twitter', founder: 'Mrs. Twitter', domain: 'tech')
+kony = Startup.new(name: 'Kony', founder: 'Mrs. Kony', domain: 'finance')
+
+investor_a = VentureCapitalist.new(name: 'A', total_worth: 1234567890)
+investor_b = VentureCapitalist.new(name: 'B', total_worth: 1000000000)
+investor_c = VentureCapitalist.new(name: 'C', total_worth: 999999999)
+investor_d = VentureCapitalist.new(name: 'D', total_worth: 800000000)
+investor_e = VentureCapitalist.new(name: 'E', total_worth: 500000000)
+investor_f = VentureCapitalist.new(name: 'F', total_worth: 100000)
+
+facebook.sign_contract(investor_a, 'seed', 100000.00)
+facebook.sign_contract(investor_b, 'seed', 100000.00)
+facebook.sign_contract(investor_c, 'seed', 100000.00)
+facebook.sign_contract(investor_d, 'seed', 100000.00)
+facebook.sign_contract(investor_e, 'seed', 100000.00)
+facebook.sign_contract(investor_f, 'seed', 100000.00)
+investor_a.offer_contract(spotify, 'seed', 100000.00)
+investor_a.offer_contract(spotify, 'seed2', 100000.00)
+investor_a.offer_contract(spotify, 'seed3', 100000.00)
+investor_a.offer_contract(kony, 'seed', 100000.00)
+investor_a.offer_contract(kony, 'seed2', 200000.00)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
