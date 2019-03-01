34c11e2514ebc6cda1423e7775dcecc54f8a473f
HemalathaMurugan <hema@Hemalathas-MBP.attlocal.net>
2019-02-28 22:07:09 -0600

    Done

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,18 @@
 class FundingRound
 
+    attr_accessor :startup, :venturecapitalist, :investment, :type
+
+    @@all = []
+
+    def initialize(startup, venturecapitalist, type, investment)
+        @startup = startup
+        @venturecapitalist = venturecapitalist
+        @type = type
+        @investment = investment
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,4 +1,6 @@
 require_relative '../config/environment.rb'
+require_relative '../app/models/funding_round'
+
 
 def reload
   load 'config/environment.rb'
@@ -6,6 +8,19 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+
+openx = Startup.new('OpenX', 'Tamilan', 'IT solutions')
+infosys = Startup.new('Infosys', 'NarayanaMoorthy','Technology solutions')
+openx.pivot('Technology and VLSI solutions',"Openx^")
+
+musk = VentureCapitalist.new('Musk', 20000000)
+warnerbros = VentureCapitalist.new('Warner Brothers Inc.', 3250000000000)
+openx.sign_contract(musk, "SeriesB",1000)
+infosys.sign_contract(warnerbros, "SeriesA", 2000)
+
+round1 = FundingRound.new(infosys, musk, 'SeriesA', 50000)
+ 
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
