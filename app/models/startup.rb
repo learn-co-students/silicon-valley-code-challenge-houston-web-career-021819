7cbd84ce801a29e95a2a4819e1925fd88d3aeeeb
github username <github email address>
2019-03-07 10:38:51 -0600

    Done

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,54 @@
 class Startup
+  attr_accessor :name
+  attr_reader :founder, :domain
+  @@all = []
 
-end
+    def initialize(name, founder, domain)
+      @name = name
+      @founder = founder
+      @domain = domain
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+    def pivot(domain, name)
+      @domain = domain
+      @name = name
+    end
+
+    def self.find_by_founder(name)
+      self.all.select {|startup| startup.founder == name}
+    end
+
+    def self.domains
+      self.all.map {|startup| startup.domain}
+
+    end
+
+
+    end
+
+
+
+
+
+   #  `Startup#name`
+   #   - returns a **string** that is the startup's name
+   # - `Startup#founder`
+   #   - returns a **string** that is the founder's name
+   #   - Once a startup is created, the founder cannot be changed.
+   # - `Startup#domain`
+   #   - returns a **string** that is the startup's domain
+   #   - Once a startup is created, the domain cannot be changed.
+   # - `Startup#pivot`
+   #   - given a string of a **domain** and a string of a **name**, change the domain and name of the startup
+   # - `Startup.all`
+   #   - should return **all** of the startup instances
+   # - `Startup.find_by_founder`
+   #   - given a string of a **founder's name**, returns the **first startup** whose founder's name matches
+   # - `Startup.domains`
+   #   - should return an **array** of all of the different startup domains
+   #
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,57 @@
-class VentureCapitalist
+class Venture_Capitalist
+  attr_accessor :name, :total_worth
+  @@all = []
 
-end
+    def initialize(name, total_worth)
+      @name = name
+      @total_worth = total_worth.to_i
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.tres_commas_club
+      self.all.select {|woman| woman.total_worth > 1000000000}
+    end
+
+
+
+  end
+
+
+
+
+
+# - `VentureCapitalist#name`
+  #   - returns a **string** that is the venture capitalist's name
+  # - `VentureCapitalist#total_worth`
+  #   - returns a **number** that is the total worth of this investor (e.g., think of it as how much money they have)
+  # - `VentureCapitalist.all`
+  #   - returns an array of all venture capitalists
+  # - `VentureCapitalist.tres_commas_club`
+  #   - returns an array of all venture capitalists in the Trés Commas club (they have more then 1,000,000,000 `total_worth`)
+
+
+
+
+# class VentureCapitalist
+#   attr_accessor :name, :total_worth
+#   @@all = []
+#
+#   def initialize(name, total_worth)
+#     self.name = name
+#     self.total_worth = total_worth
+#     @@all << self
+#   end
+#
+#   def self.all
+#     @@all
+#   end
+#
+#   def self.tres_commas_club
+#     self.all.select {|dude| dude.total_worth > 1000000000}
+#   end
+#
+# end
